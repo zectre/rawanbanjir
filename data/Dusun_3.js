042b5478c9978a454b2809224a7f8d05a32269e6
zectre <43196730+zectre@users.noreply.github.com>
2020-02-07 00:00:20 +0700

    Add files via upload

---
+++ data/Dusun_3.js
@@ -0,0 +1,19 @@
+var json_Dusun_3 = {
+"type": "FeatureCollection",
+"name": "Dusun_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nama": "Palorejo" }, "geometry": { "type": "Point", "coordinates": [ 110.849646319783034, -7.033131559187167 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Karanganyar" }, "geometry": { "type": "Point", "coordinates": [ 110.849974077806053, -7.034897913023604 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Kalimedang" }, "geometry": { "type": "Point", "coordinates": [ 110.852410694438575, -7.036715400397042 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Ngemplak" }, "geometry": { "type": "Point", "coordinates": [ 110.856703062270427, -7.040451088801803 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Karangmojo" }, "geometry": { "type": "Point", "coordinates": [ 110.850973047487585, -7.03062138644393 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Kandangan" }, "geometry": { "type": "Point", "coordinates": [ 110.845821548814499, -7.02865161386631 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Gendingan" }, "geometry": { "type": "Point", "coordinates": [ 110.839482406805971, -7.028674717711691 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Sentono" }, "geometry": { "type": "Point", "coordinates": [ 110.832537604618025, -7.031240837450944 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Donggowak" }, "geometry": { "type": "Point", "coordinates": [ 110.831021813186041, -7.028671743596878 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Ngeluk" }, "geometry": { "type": "Point", "coordinates": [ 110.830315812207857, -7.025492242822785 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Kalimati" }, "geometry": { "type": "Point", "coordinates": [ 110.830506204726518, -7.022792475695669 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nama": "Ploso" }, "geometry": { "type": "Point", "coordinates": [ 110.825746110987936, -7.021428197617958 ] } }
+]
+}
---
+++ data/TitikKumpul_5.js
@@ -0,0 +1,8 @@
+var json_TitikKumpul_5 = {
+"type": "FeatureCollection",
+"name": "TitikKumpul_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nama": "Balai Desa" }, "geometry": { "type": "Point", "coordinates": [ 110.845007153178713, -7.029875862142696 ] } }
+]
+}
